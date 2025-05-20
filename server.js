06f20ba93963f29e5f9ddf6585b0a8ff211bfc3f
HRDisruptor <kristophergarcia@outlook.com>
2025-05-20 17:56:57 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,16 +1,1 @@
-const fs = require('fs');
-const https = require('https');
-const express = require('express');
-// ...other imports
-
-const app = express();
-// all your middleware/routes...
-
-// For local dev with SSL certs
-const httpsOptions = {
-  key: fs.readFileSync('path/to/key.pem'),
-  cert: fs.readFileSync('path/to/cert.pem'),
-};
-https.createServer(httpsOptions, app).listen(443, () => {
-  console.log('HTTPS Server running');
-});
+// amigodocs.com - Copyright (c) 2025
